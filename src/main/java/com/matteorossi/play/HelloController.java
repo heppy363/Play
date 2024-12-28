79997e8cc46c604faadc8bd211444bfef5fa8499
matteo <matteorossiteo04@gmail.com>
2024-12-28 20:38:12 +0100

    Inizzializzazione proggetto

---
+++ src/main/java/com/matteorossi/play/HelloController.java
@@ -0,0 +1,14 @@
+package com.matteorossi.play;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
